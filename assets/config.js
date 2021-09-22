18b4fb877ed213f6a84b6bf45713c5eed50c515b
ricar <ricardo.v1v45@gmail.com>
2021-09-21 22:31:23 -0400

    config firebase a bit hidden, last version before first hosting

---
+++ assets/config.js
@@ -0,0 +1,12 @@
+(async ()=>{ 
+const firebaseConfig = {
+    apiKey: "AIzaSyDmLieqhU5d3x8omc0gJfYcBpYzEvTxvo0",
+    authDomain: "mitrivia77-97762.firebaseapp.com",
+    databaseURL: "https://mitrivia77-97762-default-rtdb.firebaseio.com",
+    projectId: "mitrivia77-97762",
+    storageBucket: "mitrivia77-97762.appspot.com",
+    messagingSenderId: "349194604948",
+    appId: "1:349194604948:web:870f0fc5a666e34ccaff81"
+    };
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);})();
