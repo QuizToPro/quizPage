a23454e5d3c313f504313f15db3680180c4d921c
ricar <ricardo.v1v45@gmail.com>
2021-09-21 22:45:39 -0400

    primera version hosteada

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ firebase.auth().signInAnonymously()
         .then((docRef) => {
             console.log(docRef);
             console.log("Document written with ID: ", docRef.id);
-            const pathToPlayQuiz = (collectionName === 'quiz_sugeridos') ? `./playQuiz/Quiz.html?id==${docRef.id}` : `./playQuiz/Quiz.html?id=${docRef.id}`;
+            const pathToPlayQuiz = (collectionName === 'quiz_sugeridos') ? `https://mitrivia77-97762.web.app/playQuiz/Quiz.html?id==${docRef.id}` : `https://mitrivia77-97762.web.app/playQuiz/Quiz.html?id=${docRef.id}`;
                 database.collection('scoreboards_table').doc(docRef.id).set({
                     table:[],
                     uid:uid_person,
