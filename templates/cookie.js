5434dc79d453967edaf99c9c480ec2dad1088603
QuizToPro <90007949+QuizToPro@users.noreply.github.com>
2021-10-03 17:02:10 -0400

    added cokkie

--- templates/cookie.js
+++ templates/cookie.js
@@ -1,28 +1,29 @@
-const accept = document.querySelector('.accept-cookie')
-let accepted = false;
+const $accept = document.querySelector('.accept-cookie')
+const $decline = document.querySelector('.decline-cookie')
+let $accepted = false;
 
 if(localStorage.getItem('cookie') === null){
-    console.log('hola')
     function setCookie(name, value, expires){
         let d = new Date();
         d.setTime( d.getTime() + expires * 24 * 60 * 60 * 1000 );
         document.cookie = `${name} = ${value}; expires = ${d.toUTCString()}; path=/`;
     };
     
-    accept.addEventListener('click', e => {
+    $accept.addEventListener('click', e => {
         localStorage.setItem('cookie', true)
-        accepted = true;
+        $accepted = true;
         $cookieDiv.style.display = 'none'
     });
     
     $btnStart.addEventListener('click', () => {
-        if(accepted){
+        if($accepted){
             if($userInput.value.length > 0){        
-                console.log('hola')  
                 setCookie("user", $userInput.value, 10)
             };
         };
     });
 };
 
-
+$decline.addEventListener('click', () => {
+    $cookieDiv.style.display = 'none'
+})
